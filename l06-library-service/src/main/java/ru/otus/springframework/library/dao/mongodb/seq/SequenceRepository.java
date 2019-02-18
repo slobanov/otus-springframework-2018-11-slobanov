8602e9150dcfd0329e1486005081c59d2997beba
Sergey Lobanov <s.y.lobanov@yandex.ru>
2019-02-18 23:05:59 +0300

    add @ConditionalOnProperty to SequenceRepository

--- l06-library-service/src/main/java/ru/otus/springframework/library/dao/mongodb/seq/SequenceRepository.java
+++ l06-library-service/src/main/java/ru/otus/springframework/library/dao/mongodb/seq/SequenceRepository.java
@@ -1,6 +1,7 @@
 package ru.otus.springframework.library.dao.mongodb.seq;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,7 @@ import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Repository
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "library.dao.provider", havingValue = "spring-mongodb-jpa")
 public class SequenceRepository {
 
     private final MongoOperations mongodbOps;
